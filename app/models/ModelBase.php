15d54cbe7c26340aaccb8ce8ec75df22074459db
Maing <280561543@qq.com>
2017-03-30 01:28:06 +0800

    Create: 创建model.

---
+++ app/models/ModelBase.php
@@ -0,0 +1,8 @@
+<?php
+class ModelBase extends \Phalcon\Mvc\Model {
+	public function initialize() {
+		//$this -> setConnectionService('db');
+	}
+
+}
+?>
---
+++ app/models/TuanzuSuite.php
@@ -0,0 +1,4 @@
+<?php
+class TuanzuSuite extends ModelBase {
+}
+?>
